2a04a1bea0dd6ed3826b4ccc11be130e66bd6cf2
jhonvillanueva44 <jhon.villanueva.l@tecsup.edu.pe>
2025-06-30 04:53:27 -0500

    segundo intento

--- routes/categorias.routes.js
+++ routes/categorias.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { getCategorias, createCategoria, updateCategoria, deleteCategoria } from '../controllers/categorias.controller.js';
 
 const router = Router();
-
+//hola
 router.get('/', getCategorias);
 router.post('/', createCategoria);
 router.put('/:id', updateCategoria);
